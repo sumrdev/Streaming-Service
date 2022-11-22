6ddf51bb0b89fc4b19b59285e997d12de037f4e9
Marius Nielsen <70335671+sumrdev@users.noreply.github.com>
2022-11-22 18:10:33 +0100

    ⚡Added main, and dataaccessimpl

--- src/data/DataAccessImpl.java
+++ src/data/DataAccessImpl.java
@@ -1,5 +1,56 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 class DataAccessImpl implements DataAccess {
+
+    private final String path;
+
+    public DataAccessImpl(String path) {
+        this.path = path;
+    }
+
+    // Load data from file and return as a list of strings
+    // Each string is a line from the file
+    // Throws an exception if the file is not found or if there is an error reading the file
+    @Override
+    public List<String> load(String dataField) {
+        List<String> data = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("../../database/" + dataField + ".txt"));
+            reader.lines().forEach(data::add);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO Exception while closing file");
+        }
+        return data;
+    }
+
+    // Save data to file
+    // Throws an exception if there is an error writing to the file
+    @Override
+    public void save(List<String> data) {
+        File f = new File(this.path);
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(f);
+            for (String s : data) {
+                pw.println(s);
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ src/presentation/MainWindow.java
@@ -0,0 +1,8 @@
+public class MainWindow{
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+    public MainWindow() {
+        System.out.println("Hello World");
+    }
+}
