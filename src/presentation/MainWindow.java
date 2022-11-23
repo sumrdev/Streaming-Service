74e71024daaa5638ddc9056b01515f7cf04f0002
davidfeldner <davidfeldner12@gmail.com>
2022-11-24 00:00:36 +0100

    Navbar and css

--- src/presentation/MainWindow.java
+++ src/presentation/MainWindow.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Streaming Service");
+
+        BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        root.setCenter(FXMLLoader.load(getClass().getResource("./mainMenu/mainMenu.fxml")));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
---
+++ src/presentation/loginMenu/loginMenu.java
@@ -0,0 +1,5 @@
+package presentation.loginMenu;
+
+public class loginMenu {
+
+}
