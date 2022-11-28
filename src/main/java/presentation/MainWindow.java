a39ac5623d82705ad3a8ce8efa2d4a8c19f5d576
davidfeldner <davidfeldner12@gmail.com>
2022-11-29 00:25:39 +0100

    Fix page switching

--- src/main/java/presentation/MainWindow.java
+++ src/main/java/presentation/MainWindow.java
@@ -9,32 +9,21 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
-    public ItemRegistry ir;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("Streaming Service");
 
-        BorderPane root = (BorderPane) FXMLLoader.load(getClass().getClassLoader().getResource("mainWindow.fxml"));
-        FXMLLoader menu = new FXMLLoader(getClass().getClassLoader().getResource("mainMenu.fxml"));
-        root.setCenter(menu.load());
+        FXMLLoader rootLoader = new FXMLLoader(getClass().getClassLoader().getResource("mainWindow.fxml"));
+        BorderPane root = (BorderPane) rootLoader.load();
 
-        this.ir = new ItemRegistryImpl();
-        this.ir.initialize();
-        MainMenuController menuController = menu.getController();
-        System.out.println(menuController);
-        try {
-            menuController.loadItems(ir);
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getClassLoader().getResource("style.css").toExternalForm());
-
         primaryStage.setScene(scene);
         primaryStage.show();
+        MainWindowController rootController = rootLoader.getController();
+        rootController.setup();
     }
 
     public static void main(String[] args) {
--- src/main/java/presentation/MainWindowController.java
+++ src/main/java/presentation/MainWindowController.java
@@ -1,41 +1,53 @@
 package presentation;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import domain.ItemRegistry;
 import domain.ItemRegistryImpl;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
-public class MainWindowController implements Initializable {
+public class MainWindowController {
 
     @FXML
     Button navButtonHome;
     @FXML
     Button navButtonLogin;
 
+    Parent loginMenu = null;
+    Parent loginMenuController = null;
+    Parent homeMenu = null;
+    MainMenuController homeMenuController = null;
+    BorderPane scene;
+    public ItemRegistry ir;
+
     public void navigateHome() throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainMenu.fxml"));
-        BorderPane scene = (BorderPane) navButtonHome.getScene().getRoot();
-        scene.setCenter(root);
+        scene.setCenter(homeMenu);
     }
 
     public void navigateLogin() throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("loginMenu.fxml"));
-        BorderPane scene = (BorderPane) navButtonHome.getScene().getRoot();
-        scene.setCenter(root);
+        scene.setCenter(loginMenu);
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
+    public void setup() {
+        try {
+            this.scene = (BorderPane) navButtonHome.getScene().getRoot();
+            FXMLLoader homeMenu = new FXMLLoader(getClass().getClassLoader().getResource("mainMenu.fxml"));
+            FXMLLoader loginMenu = new FXMLLoader(getClass().getClassLoader().getResource("loginMenu.fxml"));
+            this.homeMenu = homeMenu.load();
+            this.homeMenuController = homeMenu.getController();
+            this.loginMenu = loginMenu.load();
+            this.loginMenuController = loginMenu.getController();
+            this.ir = new ItemRegistryImpl();
+            this.ir.initialize();
+            this.homeMenuController.loadItems(this.ir);
+            this.navigateHome();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
