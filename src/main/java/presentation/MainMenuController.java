422064915998ba105969d65c433189889f62d96d
davidfeldner <davidfeldner12@gmail.com>
2022-11-29 11:05:02 +0100

    Simple filters

--- src/main/java/presentation/MainMenuController.java
+++ src/main/java/presentation/MainMenuController.java
@@ -1,9 +1,13 @@
 package presentation;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 import domain.ItemRegistry;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -19,21 +23,31 @@ public class MainMenuController {
     FlowPane itemGrid;
 
     @FXML
-    ChoiceBox choiceGenre;
+    ChoiceBox<String> choiceGenre;
 
     @FXML
-    ChoiceBox choiceCategory;
+    ChoiceBox<String> choiceCategory;
 
-    public void loadItems(ItemRegistry ir) {
-        ir.getMovieKeyList().forEach(movie -> {
+    ItemRegistry ir = null;
+
+    public void loadItems(ItemRegistry ir, List<String> keyList) {
+        this.ir = ir;
+        itemGrid.getChildren().clear();
+        keyList.forEach(item -> {
             try {
                 StackPane itemPane = (StackPane) FXMLLoader.load(getClass().getClassLoader().getResource("item.fxml"));
                 ImageView image = (ImageView) itemPane.getChildren().get(0);
-                Image img = new Image(getClass().getResourceAsStream("/movie_img/" + movie + ".jpg"));
+                Image img;
+                // System.out.println(item);
+                if (ir.getSeriesSeasons(item) == null) {
+                    img = new Image(getClass().getResourceAsStream("/movie_img/" + item + ".jpg"));
+                } else {
+                    img = new Image(getClass().getResourceAsStream("/show_img/" + item + ".jpg"));
+                }
                 image.setImage(img);
 
                 Text text = (Text) itemPane.getChildren().get(1);
-                text.setText(movie);
+                text.setText(item);
                 itemGrid.getChildren().add(itemPane);
 
             } catch (Exception e) {
@@ -45,5 +59,28 @@ public class MainMenuController {
     public void loadFilters(ItemRegistry ir) {
         String[] genres = ir.getGenreSet().stream().toArray(String[]::new);
         choiceGenre.getItems().addAll(genres);
+
+        choiceCategory.getItems().addAll("Movies", "Series", "All");
+        choiceCategory.getSelectionModel().selectFirst();
+    }
+
+    public void updateItems() {
+        if (ir == null)
+            return;
+        String genre = choiceGenre.getValue();
+        String category = choiceCategory.getValue();
+
+        Stream<String> items = Stream.concat(ir.getMovieKeyList().stream(), ir.getSeriesKeyList().stream());
+        if (genre != null) {
+            items = items.filter(item -> Arrays.asList(ir.getItemGenre(item)).contains(genre));
+        }
+        if (category != null) {
+            if (category.equals("Movies")) {
+                items = items.filter(item -> ir.getSeriesSeasons(item) == null);
+            } else if (category.equals("Series")) {
+                items = items.filter(item -> ir.getSeriesSeasons(item) != null);
+            }
+        }
+        loadItems(ir, items.toList());
     }
 }
