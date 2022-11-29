a4fa68b925a329b4ac76b2c3d9c07bbde80232d4
Marius Nielsen <70335671+sumrdev@users.noreply.github.com>
2022-11-29 11:05:35 +0100

    Added more tests

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -20,4 +20,32 @@ class MovieTest {
         String movieKey = movies.get(0);
         assertEquals(itemRegistry.getItemString(movieKey).toString(), "The Godfather; 1972;Crime,Drama;9.2;");
     }
+
+    @Test
+    void getMovieTitleTest() {
+        ArrayList<String> movies = itemRegistry.getMovieKeyList();
+        String movieKey = movies.get(0);
+        assertEquals(itemRegistry.getItemTitle(movieKey), "The Godfather");
+    }
+
+    @Test
+    void getMovieGenreTest() {
+        ArrayList<String> movies = itemRegistry.getMovieKeyList();
+        String movieKey = movies.get(0);
+        assertEquals(itemRegistry.getItemGenre(movieKey)[0], "Crime");
+    }
+
+    @Test
+    void getMovieRatingTest() {
+        ArrayList<String> movies = itemRegistry.getMovieKeyList();
+        String movieKey = movies.get(0);
+        assertEquals(itemRegistry.getItemRating(movieKey), 9.2);
+    }
+
+    @Test
+    void getMovieReleaseTest() {
+        ArrayList<String> movies = itemRegistry.getMovieKeyList();
+        String movieKey = movies.get(0);
+        assertEquals(itemRegistry.getItemRelease(movieKey), 1972);
+    }
 }
--- src/test/java/SeriesTest.java
+++ src/test/java/SeriesTest.java
@@ -1,3 +1,24 @@
-public class SeriesTest {
-    
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import domain.ItemRegistryImpl;
+
+class SeriesTest {
+    static ItemRegistryImpl itemRegistry;
+
+    @BeforeAll
+    static void beforeAll() {
+        itemRegistry = new ItemRegistryImpl();
+        itemRegistry.initialize();
+    }
+
+    @Test
+    void toStringTest() {
+        ArrayList<String> series = itemRegistry.getSeriesKeyList();
+        String seriesKey = series.get(0);
+        System.out.println(itemRegistry.getItemString(seriesKey).toString());
+    }
+
 }
